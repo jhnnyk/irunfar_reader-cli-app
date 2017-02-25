c9bef45cc56881511afb9fabc1d3b1603893288a
John Knotts <jhnnyk@gmail.com>
2017-02-25 15:03:47 -0700

    list articles stub

--- lib/irunfar_reader/cli.rb
+++ lib/irunfar_reader/cli.rb
@@ -2,6 +2,16 @@ class IrunfarReader::CLI
   
   def call
     puts "Welcome to the iRunFar.com Reader!"
+    list_articles
+  end
+
+  def list_articles
+    puts <<~DOC
+      1. 2017 Transgrancanaria Live Coverage
+      2. 2017 Transgrancanaria Preview
+      3. The Simple Pleasure Of The Long Run
+      4. Didrik Hermansen Pre-2017 Transgrancanaria Interview
+    DOC
   end
 
 end
