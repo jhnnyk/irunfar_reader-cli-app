6d0fc6781a80bb916fcb746034019fb9a9e20030
John Knotts <jhnnyk@gmail.com>
2017-02-26 08:03:15 -0700

    actually scraping an article

--- lib/irunfar_reader.rb
+++ lib/irunfar_reader.rb
@@ -1,3 +1,7 @@
+require 'open-uri'
+require 'nokogiri'
+require 'pry'
+
 require_relative "./irunfar_reader/version"
 require_relative './irunfar_reader/article'
 require_relative './irunfar_reader/cli'
--- lib/irunfar_reader/article.rb
+++ lib/irunfar_reader/article.rb
@@ -2,20 +2,30 @@ class IrunfarReader::Article
   attr_accessor :title, :author, :url, :content
   
   def self.all
+    self.scrape_articles
+  end
 
-    article_1 = self.new
-    article_1.title = "2017 Transgrancanaria Live Coverage"
-    article_1.author = "Bryon Powell"
-    article_1.url = "http://www.irunfar.com/2017/02/2017-transgrancanaria-live-coverage.html"
-    article_1.content = "The 2017 Transgrancanaria kicks off at 11 p.m. local time on Friday, February 24 in the Canary Islands (4 p.m. MST in the U.S.). We’ll be livecasting the event..."
+  def self.scrape_articles
+    articles = []
 
-    article_2 = self.new
-    article_2.title = "The Simple Pleasure Of The Long Run"
-    article_2.author = "Andy Jones-Wilkins"
-    article_2.url = "http://www.irunfar.com/2017/02/the-simple-pleasure-of-the-long-run.html"
-    article_2.content = "Over the past few years as ultrarunning has increased in popularity and training methods have evolved, the sport has undergone a transformation..."
+    # go to irunfar.com
+    # extract the properties
+    # instantiate an article
+    # put the article in the articles array
 
-    [article_1, article_2]
+    articles << self.scrape_irunfar
+
+    articles
+  end
+
+  def self.scrape_irunfar
+    doc = Nokogiri::HTML(open("http://www.irunfar.com/"))
+
+    article = self.new
+    article.title = doc.css(".hpblock h2 a")[0].text
+    article.author = doc.css('.hpblock p a[rel="author"]')[0].text
+    article.url = doc.css(".hpblock h2 a")[0].attribute("href").value
+    article
   end
 
 end
